4dff02d332e1200124f936a7c710edc460cb0a8e
Shafiulislam <ishafiul900@gmail.com>
2023-05-20 13:35:30 +0600

    delete id and on items bacent data create complit

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
         res.send(result);
     });
 
+    // delet data in mongodb
+    app.delete('/toys/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // get spacip data get
     app.get('/toys/:id', async(req, res) => {
